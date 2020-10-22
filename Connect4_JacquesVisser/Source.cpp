430197e4ee09cd24bac9ee2010e635a168f0473d
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-10-22 11:28:50 -0700

    Added showTitle, displayGrid and promptPlayer

--- Connect4_JacquesVisser/Source.cpp
+++ Connect4_JacquesVisser/Source.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 const int WIDTH = 7;
 const int HEIGHT = 6;
+int columnArray[WIDTH] = {};
+int rowArray[HEIGHT] = {};
 
-int main()
-{
-	int width[WIDTH] = {};
-	int height[HEIGHT] = {};
+void displayGrid() {
+
 	int w;
 	int h;
-	char row = 'A';
+	int row_letter = 64; //In ASCII 64 represents "@", the character before A,B,C etc.
 
 	cout << "  |1| |2| |3| |4| |5| |6| |7| \n";
 	cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
@@ -19,14 +20,50 @@ int main()
 	for (w = 0; w < HEIGHT; w++) {
 		for (h = 0; h < WIDTH; h++) {
 			if (h == 0) {
-				row++;
-				cout << char(row - 1);
+				row_letter++;
+				cout << char(row_letter);
 			}
-			cout << " |" << width[w] << "|";
+			cout << " |" << "." << "|";
 		}
 		cout << "\n";
 	}
+	cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+	cout << "\n";
+	cout << "\n";
+}
 
+void promptPlayer() {
+	
+	string welcome = "Hello! Welcome...";
+	string p1 = "Player 1, go!\n";
+	string p2;
+	bool turn = false;
+	
+	cout << welcome;
+
+	if (!turn) {
+		cout << p1;
+	}
+}
+
+void showTitle() {
+	cout << "{-------------------------------------}" << endl;
+	cout << "{          CONSOLE CONNECT 4          }" << endl;
+	cout << "{-------------------------------------} " << endl;
+	cout << "\n";
+	cout << "\n";
+}
+
+int main()
+{
+	char dRow;
+	int dColumn;
+
+	showTitle();
+	displayGrid();
+	promptPlayer();
 	system("pause");
 	return 0;
 }
+
+
