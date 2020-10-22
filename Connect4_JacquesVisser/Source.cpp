23a58335e2bc63caca1a4e513b03e635d3cf5dce
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-10-22 13:36:24 -0700

    Added promptPlayer function
    This function takes what ever position the player enters and separates the string into a char and an int

--- Connect4_JacquesVisser/Source.cpp
+++ Connect4_JacquesVisser/Source.cpp
@@ -6,9 +6,11 @@ using namespace std;
 const int WIDTH = 7;
 const int HEIGHT = 6;
 int columnArray[WIDTH] = {};
-int rowArray[HEIGHT] = {};
+char rowArray[HEIGHT] = {};
+int playerNumber;
 
-void displayGrid() {
+
+void displayGrid() { //Displays the 6x7 Grid 
 
 	int w;
 	int h;
@@ -17,8 +19,10 @@ void displayGrid() {
 	cout << "  |1| |2| |3| |4| |5| |6| |7| \n";
 	cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
 
-	for (w = 0; w < HEIGHT; w++) {
-		for (h = 0; h < WIDTH; h++) {
+	for (w = 0; w < HEIGHT; w++)
+	{
+		for (h = 0; h < WIDTH; h++) 
+		{
 			if (h == 0) {
 				row_letter++;
 				cout << char(row_letter);
@@ -32,18 +36,56 @@ void displayGrid() {
 	cout << "\n";
 }
 
-void promptPlayer() {
+void promptPlayer() { // Checks which players turn it is and prompts their turn accordingly
 	
 	string welcome = "Hello! Welcome...";
-	string p1 = "Player 1, go!\n";
-	string p2;
-	bool turn = false;
+	string prompt = "ENTER YOUR TURN PLAYER ";
+
+	string p1_s = "Player 1, please enter where you'd like to place your first piece! For example... B2\n";
+	string p1 = "Player 1's Turn";
+
+	string p2_s = "Player 2, please enter where you'd like to place your first piece! For example... B2\n";
+	string p2 = "Player 2's Turn";
+
+	bool turn = false; // FALSE reperestents player 1's turn, TRUE will represent player 2's turn
 	
 	cout << welcome;
 
 	if (!turn) {
-		cout << p1;
+		cout << p1_s << endl;
+		playerNumber = 1;
 	}
+	else
+	{
+		cout << p2_s;
+		playerNumber = 2;
+	}
+
+	cout << prompt << playerNumber << ": "<< endl;
+
+}
+
+void recieveInput(string desiredPosition) { //Takes the input of the current player
+
+	char desiredRow;
+	int desiredColumn;
+	size_t row;
+	size_t cascii;
+	size_t column_int;
+
+	for (unsigned int i = 0; i < desiredPosition.length(); i++) {
+		row = desiredPosition.at(0);
+		desiredRow = (char)row;
+		cascii = desiredPosition.at(1);
+		column_int = (int)cascii - 48;
+		desiredColumn = (char)column_int;
+	}
+
+	cout << "You enetred row: " << desiredRow << " and column: " << desiredColumn << endl;
+}
+
+void randExample() { // Generates a random example
+
 }
 
 void showTitle() {
@@ -56,12 +98,13 @@ void showTitle() {
 
 int main()
 {
-	char dRow;
-	int dColumn;
+	string dPos;
 
 	showTitle();
 	displayGrid();
 	promptPlayer();
+	cin >> dPos;
+	recieveInput(dPos);
 	system("pause");
 	return 0;
 }
