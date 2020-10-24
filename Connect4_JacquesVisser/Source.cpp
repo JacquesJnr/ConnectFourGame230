976b211ff4c2ad62231749b5851df436402a87a9
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-10-24 15:33:50 -0700

    added winCheck, validatePosition functions

--- Connect4_JacquesVisser/Source.cpp
+++ Connect4_JacquesVisser/Source.cpp
@@ -4,50 +4,43 @@
 
 using namespace std;
 
+struct playerInfo
+{
+	char playerName[81];
+	char playerChar;
+};
+
 const int WIDTH = 7;
 const int HEIGHT = 6;
 int gridArray[HEIGHT][WIDTH] = {};
 int playerNumber;
 bool gridUpdate = false;
-int placeToken = 0;
+bool gameOver = false;
 int currentHeight = 6;
-string playerSting;
-
-//Allows me to color the text
-void colorText() {
-	HANDLE  hConsole;
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	int col = 12;
-
-	// color your text in Windows console mode
-	// colors are 0=black 1=blue 2=green and so on to 15=white  
-	// colorattribute = foreground + background * 16
-	// to get red text on yellow use 4 + 14*16 = 228
-	// light red on yellow would be 12 + 14*16 = 236
-
-	FlushConsoleInputBuffer(hConsole);
-	SetConsoleTextAttribute(hConsole, col);
-
-	cout << "Color Text\n";
-	SetConsoleTextAttribute(hConsole, 10);
-}
 
 //Displays the 6x7 Grid 
-void displayGrid() {
-
+void displayGrid(char board[][7]) {
+	cout << "\n";
+	cout << "\n";
 	int row_letter = 64; //In ASCII 64 represents "@", the character before A,B,C etc.
-	
+
 	cout << " |1| |2| |3| |4| |5| |6| |7| \n";
 	cout << " [                         ]\n";
 
 	for (int h = 0; h < HEIGHT; h++) {
 		for (int w = 0; w < WIDTH; w++) {
+
+			if (board[h][w] != 'X' && board[h][w] != 'O')
+				board[h][w] = '.';
+
 			if (h > 0) {
 				if (w == 0) {
 					cout << endl;
+
 				}
 			}
-			cout << " |" << gridArray[h][w] << "|";
+
+			cout <<" |" << board[h][w] << "|";
 		}
 	}
 
@@ -61,82 +54,27 @@ void displayGrid() {
 	cout << "\n";
 }
 
-// Inserts the players token into the array
-void updateGrid(int columnNumber) {
-	cout << "GRID UPDATED...\n";
-	cout << "\n";
-
-	int h;
-	int w;
-	//string my_array[WIDTH][HEIGHT];
-
-	for (h = 0; h < HEIGHT; h++) {
-
-		if (gridArray[0][0] == NULL) {
-			currentHeight--;
-			cout << currentHeight << endl;
-			h = currentHeight;
-		}
-		for (w = 0; w < WIDTH; w++) {
-
-			gridArray[h][columnNumber -1] = 1;		
-		}		
-	}
-
-	recieveInput(playerSting);
-	cout  << "Height: " << currentHeight << endl;
-	cout << "Column: " << columnNumber << endl;
-}
-
-// Checks which players turn it is and prompts their turn accordingly
-void promptPlayer() { 
-	
-	int turnCount;
-
-	string welcome = "Hello! Welcome...";
-	string prompt = "ENTER YOUR TURN PLAYER ";
-
-	string p1_s = "Player 1, please enter the column number where you'd like to place your first piece! For example... 1 \n";
-	string p1 = "Player 1's Turn";
-
-	string p2_s = "Player 2, please enter where you'd like to place your first piece! For example... 2\n";
-	string p2 = "Player 2's Turn";
-
-	bool turn = false; // FALSE reperestents player 1's turn, TRUE will represent player 2's turn
-	
-	cout << welcome;
-
-	if (!turn) {
-		playerNumber = 1;
-		cout << p1_s << endl;
-	}
-	else
-	{
-		playerNumber = 2;
-		cout << p2_s;
-	}
-
-	cout << prompt << playerNumber << ": "<< endl;
-
-}
-
 //Takes the input of the current player
-void recieveInput(string desiredPosition) { 
+int promptPlayer(char board[][7], playerInfo activePlayer) {
 
-	int desiredColumn = 0;
-	size_t asciiNum;
-	size_t columnInt;
+	int dropChoice;
+	do
+	{
+		cout << activePlayer.playerName << "'s Turn ";
+		cout << "Please enter a number between 1 and 7: ";
+		cin >> dropChoice;
 
-	for (unsigned int i = 0; i < desiredPosition.length(); i++) {
-		asciiNum = desiredPosition.at(0);
-		columnInt = (int)asciiNum - 48;
-		desiredColumn = (char)columnInt;
-	}
+		while (board[1][dropChoice] == 'X' || board[1][dropChoice] == 'O')
+		{
+			cout << "That row is full, please enter a new row: ";
+			cin >> dropChoice;
+		}
 
-	placeToken = desiredColumn;
-	
+	} while (dropChoice < 1 || dropChoice > 7);
+
 	cout << "\n";
-	cout << "You entered column: " << desiredColumn << "\n" << "CONTINUE? Y/N?" << endl;
+	cout << "You entered column: " << dropChoice;
+	return dropChoice;
 }
 
 // Checks if the player wants to continue with the row they've entered
@@ -151,7 +89,7 @@ char validateInput(char yesNo) {
 	{
 		cout << "Updating Grid\n";
 		system("cls");
-		gridUpdate = true;		
+		gridUpdate = true;
 		cout << "\n";
 		break;
 	}
@@ -172,6 +110,136 @@ char validateInput(char yesNo) {
 	return yesNo;
 }
 
+void validatePosition(char board[][7], playerInfo activePlayer, int tokenPosition) {
+	
+	int length, turn;
+	length = 6;
+	turn = 0;
+
+	do
+	{
+		if (board[length][tokenPosition - 1] != 'X' && board[length][tokenPosition -1] != 'O')
+		{
+			board[length][tokenPosition -1] = activePlayer.playerChar;
+			turn = 1;
+		}
+		else
+			--length;
+	} while (turn != 1);
+
+}
+
+// Inserts the players token into the array
+void updateGrid(int columnNumber) {
+	cout << "GRID UPDATED...\n";
+	cout << "\n";
+
+	int h;
+	int w;
+	//string my_array[WIDTH][HEIGHT];
+
+	for (h = 0; h < HEIGHT; h++) {
+
+		if (gridArray[0][0] == NULL) {
+			currentHeight--;
+			cout << currentHeight << endl;
+			h = currentHeight;
+		}
+		for (w = 0; w < WIDTH; w++) {
+
+			gridArray[h][columnNumber - 1] = 1;
+		}
+	}
+
+	cout << "Height: " << currentHeight << endl;
+	cout << "Column: " << columnNumber << endl;
+}
+
+int winCheck(char board[][7], playerInfo activePlayer) {
+	char activeChar;
+	int win;
+
+	activeChar = activePlayer.playerChar;
+	win = 0;
+
+	for (int i = 8; i >= 1; --i)
+	{
+
+		for (int ix = 9; ix >= 1; --ix)
+		{
+
+			if (board[i][ix] == activeChar &&
+				board[i - 1][ix - 1] == activeChar &&
+				board[i - 2][ix - 2] == activeChar &&
+				board[i - 3][ix - 3] == activeChar)
+			{
+				win = 1;
+			}
+
+
+			if (board[i][ix] == activeChar &&
+				board[i][ix - 1] == activeChar &&
+				board[i][ix - 2] == activeChar &&
+				board[i][ix - 3] == activeChar)
+			{
+				win = 1;
+			}
+
+			if (board[i][ix] == activeChar &&
+				board[i - 1][ix] == activeChar &&
+				board[i - 2][ix] == activeChar &&
+				board[i - 3][ix] == activeChar)
+			{
+				win = 1;
+			}
+
+			if (board[i][ix] == activeChar &&
+				board[i - 1][ix + 1] == activeChar &&
+				board[i - 2][ix + 2] == activeChar &&
+				board[i - 3][ix + 3] == activeChar)
+			{
+				win = 1;
+			}
+
+			if (board[i][ix] == activeChar &&
+				board[i][ix + 1] == activeChar &&
+				board[i][ix + 2] == activeChar &&
+				board[i][ix + 3] == activeChar)
+			{
+				win = 1;
+			}
+		}
+
+	}
+
+	return win;
+}
+
+void playerWin(playerInfo activePlayer) {
+	cout << endl << activePlayer.playerName << " Connected Four, You Win!" << endl;
+}
+
+
+//Allows me to color the text
+void colorText() {
+	HANDLE  hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	int col = 12;
+
+	// color your text in Windows console mode
+	// colors are 0=black 1=blue 2=green and so on to 15=white  
+	// colorattribute = foreground + background * 16
+	// to get red text on yellow use 4 + 14*16 = 228
+	// light red on yellow would be 12 + 14*16 = 236
+
+	FlushConsoleInputBuffer(hConsole);
+	SetConsoleTextAttribute(hConsole, col);
+
+	cout << "Color Text\n";
+	SetConsoleTextAttribute(hConsole, 10);
+}
+
+
 // Does what it says on the tin
 void showTitle() {
 	cout << "[-------------------------------------]" << endl;
@@ -181,39 +249,55 @@ void showTitle() {
 	cout << "\n";
 }
 
-class Game {
-private:
-	char myArray[HEIGHT][WIDTH] = {};
-
-public:
-
-	void generateGrid() {
-
-	}
-
-};
 
 int main()
 {
-	string dPos;
-	char my_option;
+	char board[HEIGHT][WIDTH];
+	int tokenPos, winCondition, drawCondition,  restart;
+	char cont;
+	playerInfo P1, P2;
+
+	winCondition = 0;
+	drawCondition = 0;
+	restart = 0;
 
 	showTitle();
+	cout << "Player 1, Input your name please: ";
+	cin >> P1.playerName;
+	P1.playerChar = 'X';
 
-	displayGrid();
+	cout << "Player 2, Input your name please: ";
+	cin >> P2.playerName;
+	P2.playerChar = 'O';
 
-	do {
-		promptPlayer();
-		cin >> dPos;
-		recieveInput(dPos);
-		cin >> my_option;
-		validateInput(my_option);
-	} while (!gridUpdate);
-	gridUpdate = false;
+	displayGrid(board);
 	
+	do
+	{
+		tokenPos = promptPlayer(board, P1);
+		validatePosition(board, P1, tokenPos);
+		system("cls");
+		showTitle();
+		displayGrid(board);
+		winCondition = winCheck(board, P1);
+		if (winCondition == 1) {
+			playerWin(P1);
+		}
+		
+		tokenPos = promptPlayer(board, P2);
+		validatePosition(board, P2, tokenPos);
+		winCondition = winCheck(board, P2);
+		system("cls");
+		showTitle();
+		displayGrid(board);
+		if (winCondition == 1) {
+			playerWin(P2);
+		}
+
+	} while (true);
 
 
-	
+
 
 	system("pause");
 	return 0;
