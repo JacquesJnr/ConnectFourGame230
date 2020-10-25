ba585aeea58b7fca6a23c12e589233d53e3f15ec
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-10-24 18:36:08 -0700

    Made a global #define for array size,  changed some formatting outputs

--- Connect4_JacquesVisser/Source.cpp
+++ Connect4_JacquesVisser/Source.cpp
@@ -1,165 +1,177 @@
 #include <iostream>
-#include <string>
-#include <windows.h>
-
 using namespace std;
 
-struct playerInfo
+struct Players
 {
-	char playerName[81];
-	char playerChar;
+	string playerName;
+	char playerID;
 };
 
-const int WIDTH = 7;
-const int HEIGHT = 6;
-int gridArray[HEIGHT][WIDTH] = {};
-int playerNumber;
-bool gridUpdate = false;
-bool gameOver = false;
-int currentHeight = 6;
+#define WIDTH 7
+#define HEIGHT 6
+void showTitle();
+int recieveInput(char board[][WIDTH], Players activePlayer);
+void validateInput(char board[][WIDTH], Players activePlayer, int dropChoice);
+void displayGrid(char board[][WIDTH]);
+int validatePosition(char board[][WIDTH], Players activePlayer);
+int isBoardFull(char board[][WIDTH]);
+void YouWin(Players activePlayer);
+int restart(char board[][WIDTH]);
 
-//Displays the 6x7 Grid 
-void displayGrid(char board[][7]) {
-	cout << "\n";
-	cout << "\n";
-	int row_letter = 64; //In ASCII 64 represents "@", the character before A,B,C etc.
+int main()
+{
+	Players P1, P2;
+	char board[HEIGHT][WIDTH];
+	int dropChoice, win, full, again;
 
-	cout << " |1| |2| |3| |4| |5| |6| |7| \n";
-	cout << " [                         ]\n";
+	showTitle();
+	cout << "Player One please enter your name: ";
+	cin >> P1.playerName;
+	cout << endl;
+	P1.playerID = 'X';
+	cout << "Player Two please enter your name: ";
+	cin >> P2.playerName;
+	cout << endl;
+	P2.playerID = 'O';
 
-	for (int h = 0; h < HEIGHT; h++) {
-		for (int w = 0; w < WIDTH; w++) {
-
-			if (board[h][w] != 'X' && board[h][w] != 'O')
-				board[h][w] = '.';
-
-			if (h > 0) {
-				if (w == 0) {
-					cout << endl;
-
-				}
+	full = 0;
+	win = 0;
+	again = 0;
+	displayGrid(board);
+	do
+	{
+		dropChoice = recieveInput(board, P1);
+		validateInput(board, P1, dropChoice);
+		system("cls");
+		showTitle();
+		cout << ">>>>>> " << P2.playerName << "'s Turn " << "<<<<<<<<" << "\n \n";
+		displayGrid(board);
+		win = validatePosition(board, P1);
+		if (win == 1)
+		{
+			YouWin(P1);
+			again = restart(board);
+			if (again == 2)
+			{
+				break;
 			}
-
-			cout <<" |" << board[h][w] << "|";
 		}
-	}
 
-	cout << "\n";
-	cout << " [_________________________]\n";
-	cout << "\n";
-	cout << "Empty = .\n";
-	cout << "Player 1 = X\n";
-	cout << "Player 2 = O\n";
+		dropChoice = recieveInput(board, P2);
+		validateInput(board, P2, dropChoice);
+		system("cls");
+		showTitle();
+		cout << ">>>>>> " << P2.playerName << "'s Turn " << "<<<<<<<<" << "\n \n";
+		cout << "\n";
+		displayGrid(board);
+		win = validatePosition(board, P2);
+		if (win == 1)
+		{
+			YouWin(P2);
+			again = restart(board);
+			if (again == 2)
+			{
+				break;
+			}
+		}
+		full = isBoardFull(board);
+		if (full == WIDTH)
+		{
+			cout << "The board is full, it is a draw!" << endl;
+			again = restart(board);
+		}
+
+	} while (again != 2);
+
+
+
+	return 0;
+}
+
+void showTitle() {
+	cout << "[-------------------------------------]" << endl;
+	cout << "[          CONSOLE CONNECT 4          ]" << endl;
+	cout << "[-------------------------------------] " << endl;
 	cout << "\n";
 	cout << "\n";
 }
 
-//Takes the input of the current player
-int promptPlayer(char board[][7], playerInfo activePlayer) {
-
+int recieveInput(char board[][WIDTH], Players activePlayer)
+{
 	int dropChoice;
 	do
 	{
-		cout << activePlayer.playerName << "'s Turn ";
-		cout << "Please enter a number between 1 and 7: ";
+
+		cout << activePlayer.playerName << ", " << "Please enter a number between 1 and 7: ";
 		cin >> dropChoice;
 
 		while (board[1][dropChoice] == 'X' || board[1][dropChoice] == 'O')
 		{
 			cout << "That row is full, please enter a new row: ";
 			cin >> dropChoice;
+			
+			if (cin.fail()) {
+				cout << "///////////\n";
+				cout << "Not an int!\n";
+			}
 		}
 
-	} while (dropChoice < 1 || dropChoice > 7);
+	} while (dropChoice < 1 || dropChoice > WIDTH);
 
-	cout << "\n";
-	cout << "You entered column: " << dropChoice;
 	return dropChoice;
 }
 
-// Checks if the player wants to continue with the row they've entered
-char validateInput(char yesNo) {
-
-	int optionLetter = (int)yesNo;
-
-	switch (optionLetter)
-	{
-	case(89):
-	case (121):
-	{
-		cout << "Updating Grid\n";
-		system("cls");
-		gridUpdate = true;
-		cout << "\n";
-		break;
-	}
-	case(78):
-	case(110):
-	{
-		cout << "Starting over\n";
-		gridUpdate = false;
-		system("cls");
-		break;
-	}
-	default:
-		gridUpdate = false;
-		cout << "Invalid input, enter Y or y, N or n";
-		break;
-	}
-
-	return yesNo;
-}
-
-void validatePosition(char board[][7], playerInfo activePlayer, int tokenPosition) {
-	
+void validateInput(char board[][WIDTH], Players activePlayer, int dropChoice)
+{
 	int length, turn;
 	length = 6;
 	turn = 0;
 
 	do
 	{
-		if (board[length][tokenPosition - 1] != 'X' && board[length][tokenPosition -1] != 'O')
+		if (board[length][dropChoice] != 'X' && board[length][dropChoice] != 'O')
 		{
-			board[length][tokenPosition -1] = activePlayer.playerChar;
+			board[length][dropChoice] = activePlayer.playerID;
 			turn = 1;
 		}
 		else
 			--length;
 	} while (turn != 1);
 
+
 }
 
-// Inserts the players token into the array
-void updateGrid(int columnNumber) {
-	cout << "GRID UPDATED...\n";
+void displayGrid(char board[][WIDTH])
+{
+	int rows = 6, columns = 7, i, ix;
+
+	cout << " |1| |2| |3| |4| |5| |6| |7| \n";
+	cout << " [                         ]\n";
 	cout << "\n";
+	for (i = 1; i <= rows; i++)
+	{
+		for (ix = 1; ix <= columns; ix++)
+		{
+			if (board[i][ix] != 'X' && board[i][ix] != 'O')
+				board[i][ix] = '*';
 
-	int h;
-	int w;
-	//string my_array[WIDTH][HEIGHT];
+			cout << " |"<< board[i][ix] << "|";
 
-	for (h = 0; h < HEIGHT; h++) {
-
-		if (gridArray[0][0] == NULL) {
-			currentHeight--;
-			cout << currentHeight << endl;
-			h = currentHeight;
 		}
-		for (w = 0; w < WIDTH; w++) {
 
-			gridArray[h][columnNumber - 1] = 1;
-		}
+		cout << endl;
 	}
 
-	cout << "Height: " << currentHeight << endl;
-	cout << "Column: " << columnNumber << endl;
+	cout << " [_________________________]\n";
+	cout << "\n";
 }
 
-int winCheck(char board[][7], playerInfo activePlayer) {
-	char activeChar;
+int validatePosition(char board[][WIDTH], Players activePlayer)
+{
+	char XO;
 	int win;
 
-	activeChar = activePlayer.playerChar;
+	XO = activePlayer.playerID;
 	win = 0;
 
 	for (int i = 8; i >= 1; --i)
@@ -168,43 +180,43 @@ int winCheck(char board[][7], playerInfo activePlayer) {
 		for (int ix = 9; ix >= 1; --ix)
 		{
 
-			if (board[i][ix] == activeChar &&
-				board[i - 1][ix - 1] == activeChar &&
-				board[i - 2][ix - 2] == activeChar &&
-				board[i - 3][ix - 3] == activeChar)
+			if (board[i][ix] == XO &&
+				board[i - 1][ix - 1] == XO &&
+				board[i - 2][ix - 2] == XO &&
+				board[i - 3][ix - 3] == XO)
 			{
 				win = 1;
 			}
 
 
-			if (board[i][ix] == activeChar &&
-				board[i][ix - 1] == activeChar &&
-				board[i][ix - 2] == activeChar &&
-				board[i][ix - 3] == activeChar)
+			if (board[i][ix] == XO &&
+				board[i][ix - 1] == XO &&
+				board[i][ix - 2] == XO &&
+				board[i][ix - 3] == XO)
 			{
 				win = 1;
 			}
 
-			if (board[i][ix] == activeChar &&
-				board[i - 1][ix] == activeChar &&
-				board[i - 2][ix] == activeChar &&
-				board[i - 3][ix] == activeChar)
+			if (board[i][ix] == XO &&
+				board[i - 1][ix] == XO &&
+				board[i - 2][ix] == XO &&
+				board[i - 3][ix] == XO)
 			{
 				win = 1;
 			}
 
-			if (board[i][ix] == activeChar &&
-				board[i - 1][ix + 1] == activeChar &&
-				board[i - 2][ix + 2] == activeChar &&
-				board[i - 3][ix + 3] == activeChar)
+			if (board[i][ix] == XO &&
+				board[i - 1][ix + 1] == XO &&
+				board[i - 2][ix + 2] == XO &&
+				board[i - 3][ix + 3] == XO)
 			{
 				win = 1;
 			}
 
-			if (board[i][ix] == activeChar &&
-				board[i][ix + 1] == activeChar &&
-				board[i][ix + 2] == activeChar &&
-				board[i][ix + 3] == activeChar)
+			if (board[i][ix] == XO &&
+				board[i][ix + 1] == XO &&
+				board[i][ix + 2] == XO &&
+				board[i][ix + 3] == XO)
 			{
 				win = 1;
 			}
@@ -215,11 +227,7 @@ int winCheck(char board[][7], playerInfo activePlayer) {
 	return win;
 }
 
-void playerWin(playerInfo activePlayer) {
-	cout << endl << activePlayer.playerName << " Connected Four, You Win!" << endl;
-}
-
-int fullBoard(char board[][7])
+int isBoardFull(char board[][WIDTH])
 {
 	int full;
 	full = 0;
@@ -232,72 +240,28 @@ int fullBoard(char board[][7])
 	return full;
 }
 
-// Does what it says on the tin
-void showTitle() {
-	cout << "[-------------------------------------]" << endl;
-	cout << "[          CONSOLE CONNECT 4          ]" << endl;
-	cout << "[-------------------------------------] " << endl;
-	cout << "\n";
-	cout << "\n";
-}
-
-
-int main()
+void YouWin(Players activePlayer)
 {
-	char board[HEIGHT][WIDTH];
-	int tokenPos, winCondition, drawCondition,  restart;
-	char cont;
-	playerInfo P1, P2;
-
-	winCondition = 0;
-	drawCondition = 0;
-	restart = 0;
-
-	showTitle();
-	cout << "Player 1, Input your name please: ";
-	cin >> P1.playerName;
-	P1.playerChar = 'X';
-
-	cout << "Player 2, Input your name please: ";
-	cin >> P2.playerName;
-	P2.playerChar = 'O';
-
-	displayGrid(board);
-	
-	do
-	{
-		tokenPos = promptPlayer(board, P1);
-		validatePosition(board, P1, tokenPos);
-		system("cls");
-		showTitle();
-		displayGrid(board);
-		winCondition = winCheck(board, P1);
-		if (winCondition == 1) {
-			playerWin(P1);
-		}
-		
-		tokenPos = promptPlayer(board, P2);
-		validatePosition(board, P2, tokenPos);
-		winCondition = winCheck(board, P2);
-		system("cls");
-		showTitle();
-		displayGrid(board);
-		if (winCondition == 1) {
-			playerWin(P2);
-		}
-		drawCondition = fullBoard(board);
-		if (drawCondition == 7) {
-			cout << "The board is full, restarting" << endl;
-			//restart = 
-		}
-
-	} while (true);
-
-
-
-
-	system("pause");
-	return 0;
+	cout << endl << activePlayer.playerName << " Connected Four, You Win!" << endl;
 }
 
+int restart(char board[][WIDTH])
+{
+	int restart;
 
+	cout << "Would you like to restart? Yes(1) No(2): ";
+	cin >> restart;
+	if (restart == 1)
+	{
+		for (int i = 1; i <= 6; i++)
+		{
+			for (int ix = 1; ix <= 7; ix++)
+			{
+				board[i][ix] = '*';
+			}
+		}
+	}
+	else
+		cout << "Goodbye!" << endl;
+	return restart;
+}
